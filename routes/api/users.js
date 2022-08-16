2210cc5ee13baae85aa61bab86dbed48591509d5
Glasgalas <alex.starichenko@icloud.com>
2022-08-17 00:22:29 +0300

    fix path

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,6 +9,8 @@ router.get("/google", ctrlWrapper(ctrl.googleAuth));
 // redirect
 router.get("/google-redirect", ctrlWrapper(ctrl.googleRedirect));
 // carrent user
-router.post("/google-user", ctrlWrapper(ctrl.getCurrentGoogleUser));
+router.post("/google-user", ctrlWrapper(ctrl.getUser));
+// вихід
+router.get("/logout", auth, ctrlWrapper(ctrl.logout));
 
 module.exports = router;
